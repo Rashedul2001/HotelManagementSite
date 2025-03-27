f9918ffd1ac3f933a077ea366f6fa655a9c80066
Rashedul2001 <hacker360sr@gmail.com>
2025-03-28 01:07:07 +0600

    First Commit First CheckPoint

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HotelManagementSite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
